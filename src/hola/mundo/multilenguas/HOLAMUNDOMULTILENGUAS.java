56277c650540dbf7cf7a0d3bf98214d1feb859f9
luciano9 <america_virgo1993@hotmail.com>
2012-10-10 12:43:16 -0500

    Agregar proyecto de netbeans

---
+++ src/hola/mundo/multilenguas/HOLAMUNDOMULTILENGUAS.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo.multilenguas;
+
+/**
+ *
+ * @author ALVAREZ
+ */
+public class HOLAMUNDOMULTILENGUAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
